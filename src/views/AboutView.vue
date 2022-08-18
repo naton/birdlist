e4f21173430fcc49f07bc0c2dcee0ff6122cbf17
Anton Andreasson <anton.andreasson@tickster.com>
2022-08-18 16:32:58 +0200

    CSS cleanup, add logo

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1><img src="../assets/logo.webp" alt="Birdlist" class="logo"/></h1>
+    <h2>
+      Log your bird observations.<br />
+      Nothing more.
+    </h2>
   </div>
 </template>
+
+<style>
+.about {
+  display: grid;
+  place-items: center;
+  padding: 1rem;
+  align-content: center;
+}
+
+.about .logo {
+  display: flex;
+  max-width: 70%;
+  margin: 0 auto 2rem;
+}
+
+@media (prefers-color-scheme: dark) {
+  .about .logo {
+    filter: invert();
+  }
+}
+</style>
