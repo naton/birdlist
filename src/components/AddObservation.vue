2b0ebc6cccb511bd3fef6975b1d3800e34e95bc8
Anton Andreasson <anton.andreasson@tickster.com>
2022-08-19 15:07:37 +0200

    Move and rename some styling

--- src/components/AddObservation.vue
+++ src/components/AddObservation.vue
@@ -9,16 +9,22 @@ const emit = defineEmits(["change"]);
       list="birds"
       @change="emit('add', $event)"
       autocomplete="off"
+      placeholder="Enter bird name…"
     />
   </div>
 </template>
 
-<style>
-#addbird input {
+<style scoped>
+input {
   width: calc(100% - 2rem);
   margin: 0 1rem;
   padding: 0.3em;
+  border: 1px solid var(--color-border);
   box-sizing: border-box;
   font-size: 1.2rem;
+}
+
+input::placeholder {
+  color: var(--color-text);
 }
 </style>
