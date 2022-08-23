a8f5b7d086085f1d40de1ca6d3a81d3f493d4016
Anton Andreasson <anton.andreasson@tickster.com>
2022-08-23 12:52:03 +0200

    Use Dexie to get and store observations

--- src/components/ObservationsList.vue
+++ src/components/ObservationsList.vue
@@ -1,27 +1,45 @@
 <script setup>
-import { ref, computed } from "vue";
+import { toRefs, reactive, computed, onUnmounted } from "vue";
+import { liveQuery } from "dexie";
+import { db } from "../db";
 import AddObservation from "@/components/AddObservation.vue";
 import TabsList from "@/components/TabsList.vue";
 import ObservationItem from "./ObservationItem.vue";
 import BirdsList from "@/components/BirdsList.vue";
 
-let allObservations = ref([]);
-let currentMonth = ref(new Date().getMonth());
+const currentDate = reactive({ month: new Date().getMonth() });
+const allObservations = reactive({ observations: [], observationsError: null });
+const queryRefs = toRefs(allObservations);
+const subscription = liveQuery(() => db.observations.toArray()).subscribe(
+  (observations) => {
+    // Success result:
+    queryRefs.observations.value = observations;
+    queryRefs.observationsError.value = null;
+  },
+  (error) => {
+    // Error result:
+    queryRefs.observationsError.value = error;
+  }
+);
 
-const allThisMonth = computed(() =>
-  allObservations.value.filter(
+onUnmounted(() => {
+  subscription.unsubscribe();
+});
+
+const allThisMonth = computed(() => {
+  return allObservations.observations.filter(
     (obs) =>
       obs.date.getFullYear() == new Date().getFullYear() &&
-      obs.date.getMonth() == currentMonth.value
-  )
-);
+      obs.date.getMonth() == currentDate.month
+  );
+});
 
 const uniqueThisMonth = computed(() =>
   [...new Set(allThisMonth.value.map((item) => item.name))].sort()
 );
 
-const currentDate = computed(() => {
-  const date = new Date().setMonth(currentMonth.value);
+const activeMonth = computed(() => {
+  const date = new Date().setMonth(currentDate.month);
   return new Intl.DateTimeFormat("sv", {
     year: "numeric",
     month: "long",
@@ -34,82 +52,26 @@ const scrollToBottom = (el) => {
     .scrollTo(0, document.querySelector(el).scrollHeight + 20);
 };
 
-const tabList = ref(["Månadskryss", "Alla observationer"]);
+const tabList = ["Månadskryss", "Alla observationer"];
 
-let db = ref(null).value;
+const addObservation = async (ev) => {
+  try {
+    await db.observations.add({
+      name: ev.target.value,
+      date: new Date(),
+    });
 
-const getObservations = () => {
-  let transaction = db.transaction("observations", "readonly");
-  let observations = transaction.objectStore("observations");
-  let request = observations.getAll();
-
-  request.onsuccess = () => {
-    allObservations.value = request.result || [];
-  };
-};
-
-const addObservation = (ev) => {
-  let transaction = db.transaction("observations", "readwrite");
-  let observations = transaction.objectStore("observations");
-  let observation = {
-    id: Math.random(),
-    name: ev.target.value,
-    date: new Date(),
-  };
-  let addRequest = observations.add(observation);
-
-  addRequest.onsuccess = () => {
-    allObservations.value.push(observation);
     ev.target.value = ""; // reset form field value
     setTimeout(() => {
       scrollToBottom(".body");
     }, 100);
-  };
-
-  addRequest.onerror = () => {
-    console.log("Error", addRequest.error);
-  };
-};
-
-const deleteObservation = (id) => {
-  let transaction = db.transaction("observations", "readwrite");
-  let observations = transaction.objectStore("observations");
-  let getRequest = observations.getKey(id);
-
-  getRequest.onsuccess = () => {
-    let id = getRequest.result;
-    let deleteRequest = observations.delete(id);
-    deleteRequest.onsuccess = () => {
-      getObservations();
-    };
-  };
-};
-
-// Indexed DB stuff
-let openRequest = indexedDB.open("birdlist", 1);
-
-openRequest.onupgradeneeded = () => {
-  // triggers if the client had no database
-  db = openRequest.result;
-  if (!db.objectStoreNames.contains("observations")) {
-    // if there's no "observations" store
-    db.createObjectStore("observations", { keyPath: "id" }); // create it
+  } catch (error) {
+    console.log("Error", error);
   }
 };
 
-openRequest.onerror = () => {
-  console.error("Error", openRequest.error);
-};
-
-openRequest.onsuccess = () => {
-  db = openRequest.result;
-
-  db.onversionchange = () => {
-    db.close();
-    alert("Database is outdated, please reload the page.");
-  };
-
-  getObservations();
+const deleteObservation = async (id) => {
+  db.observations.delete(id);
 };
 </script>
 
@@ -118,11 +80,11 @@ openRequest.onsuccess = () => {
     <tabs-list :tabList="tabList">
       <template v-slot:tabPanel-1>
         <div class="month-nav">
-          <button @click="currentMonth--">«</button>
+          <button @click="currentDate.month--">«</button>
           <h2>
-            {{ currentDate }}
+            {{ activeMonth }}
           </h2>
-          <button @click="currentMonth++">»</button>
+          <button @click="currentDate.month++">»</button>
         </div>
         <ul>
           <ObservationItem
@@ -149,7 +111,7 @@ openRequest.onsuccess = () => {
       <template v-slot:tabPanel-2>
         <ol>
           <ObservationItem
-            v-for="item in allObservations"
+            v-for="item in allObservations.observations"
             :item="item"
             :key="item.id"
             @delete="deleteObservation"
