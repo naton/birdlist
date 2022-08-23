54b64ead61d85bf9159ef95efcf646cfa82f2ecf
Anton Andreasson <anton.andreasson@tickster.com>
2022-08-23 17:41:12 +0200

    Added Dexie Cloud integration

--- src/db.js
+++ src/db.js
@@ -1,6 +1,12 @@
 import Dexie from "dexie";
+import dexieCloud from "dexie-cloud-addon";
 
-export const db = new Dexie("birdlist");
+export const db = new Dexie("birdlist", { addons: [dexieCloud] });
 db.version(1).stores({
-  observations: "++id, name, date", // Primary key and indexed props
+  observations: "@id, name, date", // Primary key and indexed props
+});
+
+db.cloud.configure({
+  databaseUrl: "https://zyh2ho4s6.dexie.cloud",
+  requireAuth: true,
 });
