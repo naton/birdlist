56efb9c1811471922a154de0c6a0f91cf3d9cdea
Anton Andreasson <anton.andreasson@tickster.com>
2022-08-25 08:43:52 +0200

    Refactor list components

--- src/components/TabsList.vue
+++ src/components/TabsList.vue
@@ -6,32 +6,46 @@ const activeTab = ref(1);
 </script>
 
 <template>
-  <ul class="c-tabs">
-    <li
-      v-for="(tab, index) in tabList"
-      :key="index"
-      class="c-tabs__tab"
-      :class="{ 'c-tabs__tab--active': index + 1 === activeTab }"
-    >
-      <input
-        type="radio"
-        name="tabs"
-        :id="`tab-${index}`"
-        :value="index + 1"
-        v-model="activeTab"
-        hidden
-      />
-      <label :for="`tab-${index}`" v-text="tab" />
-    </li>
-  </ul>
+  <nav class="body-nav">
+    <ul class="c-tabs">
+      <li
+        v-for="(tab, index) in tabList"
+        :key="index"
+        class="c-tabs__tab"
+        :class="{ 'c-tabs__tab--active': index + 1 === activeTab }"
+      >
+        <input
+          type="radio"
+          name="tabs"
+          :id="`tab-${index}`"
+          :value="index + 1"
+          v-model="activeTab"
+          hidden
+        />
+        <label :for="`tab-${index}`" v-text="tab" />
+      </li>
+    </ul>
+  </nav>
   <template v-for="(tab, index) in tabList">
-    <div :key="index" v-if="index + 1 === activeTab">
+    <div class="body-content" :key="index" v-if="index + 1 === activeTab">
       <slot :name="`tabPanel-${index + 1}`" />
     </div>
   </template>
 </template>
 
 <style>
+.body-nav {
+  grid-area: body-nav;
+  display: flex;
+  max-width: 100vw;
+  overflow: hidden;
+  background: var(--color-background-dim);
+}
+
+.body-content {
+  grid-area: body-content;
+}
+
 .c-tabs {
   display: flex;
   list-style: none;
@@ -39,7 +53,7 @@ const activeTab = ref(1);
   white-space: nowrap;
   padding: 0;
   overscroll-behavior: none;
-  background: var(--color-background-dim);
+  flex: 3;
 }
 
 .c-tabs__tab label {
