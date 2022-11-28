ab08eba80027a3cf5d8296d56624cdba4455c8b8
Anton Andreasson <anton.andreasson@tickster.com>
2022-11-28 08:14:25 +0100

    Revert some routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/home",
+      path: "/",
+      alias: "/home",
       name: "home",
-      component: () => import("../views/HomeView.vue"),
+      component: HomeView,
     },
     {
-      path: "/",
-      alias: "/about",
+      path: "/about",
       name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
