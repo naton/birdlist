d197ae74e6844424e09aba7c39d887f68386dab1
Anton Andreasson <anton.andreasson@tickster.com>
2022-12-02 14:31:11 +0100

    Added POI icon, trim at save

--- src/components/EditDialog.vue
+++ src/components/EditDialog.vue
@@ -38,7 +38,7 @@ function deleteAndClose(id) {
 }
 
 async function save() {
-  const name = currentName.value;
+  const name = currentName.value.trim();
   const date = new Date(currentDate.value);
   const listId = selectedList.value;
   await db.observations.update(props.observation, {
@@ -58,14 +58,46 @@ function saveAndClose() {
   <div class="dialog" v-if="props.isOpen">
     <h2>{{ observation.name }}</h2>
     <h3 class="margin-bottom">{{ formatDate(observation.date) }}</h3>
+    <div v-if="observation.location" class="margin-bottom">
+      <a
+        :href="`https://www.openstreetmap.org/#map=16/${observation.location.replace(
+          ',',
+          '/'
+        )}`"
+        target="_blank"
+        class="poi"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          width="24"
+          height="24"
+        >
+          <g
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path
+              d="M8 15.5s-5.5-5-5.5-9.5a5.5 5.5 0 0 1 11 0 6.883 6.883 0 0 1-.322 2"
+            />
+            <circle cx="8" cy="6" r="1.5" />
+            <path d="m8.5 11.5 2 2 4-4" />
+          </g>
+        </svg>
+        Visa plats på karta
+      </a>
+    </div>
+
     <details v-if="canEdit(observation.owner)" class="margin-bottom">
       <summary>Redigera observation</summary>
-      <div>
+      <div class="margin-bottom">
         <label for="obs-date">Ändra namn</label>
         <input id="obs-name" type="text" v-model="currentName" />
       </div>
 
-      <div>
+      <div class="margin-bottom">
         <label for="obs-date">Ändra datum</label>
         <input
           id="obs-date"
@@ -75,7 +107,7 @@ function saveAndClose() {
         />
       </div>
 
-      <div>
+      <div class="margin-bottom">
         <label for="obs-list">Ändra lista</label>
         <select id="obs-list" @change="selectedList = $event.target.value">
           <option value="">Ingen speciell lista</option>
@@ -90,16 +122,6 @@ function saveAndClose() {
         </select>
       </div>
     </details>
-    <p v-if="observation.location">
-      <a
-        :href="`https://www.openstreetmap.org/#map=16/${observation.location.replace(
-          ',',
-          '/'
-        )}`"
-        target="_blank"
-        >Visa plats på karta</a
-      >
-    </p>
 
     <div v-if="canEdit(observation.owner)">
       <button type="button" class="secondary" @click="saveAndClose">
@@ -120,25 +142,30 @@ function saveAndClose() {
   </div>
 </template>
 
-<style scoped>
-h2 {
+<style>
+.dialog h2 {
   font-weight: bold;
 }
 
-div {
-  margin-bottom: 1rem;
-}
-
-label {
+.dialog label {
   display: block;
   margin-top: 1rem;
   font-weight: bold;
 }
 
-select,
-input {
+.dialog select,
+.dialog input {
   max-width: 90%;
   font-size: 1.5rem;
+}
+
+.dialog .poi {
+  color: var(--color-link);
+  text-decoration: none;
+}
+
+.dialog .poi svg {
+  vertical-align: top;
 }
 
 .margin-bottom {
