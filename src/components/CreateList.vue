a58239ad15c71a4b28aa75bde48720d365a0071b
Anton Andreasson <anton.andreasson@tickster.com>
2022-12-07 16:07:44 +0100

    Add support for list descriptions

--- src/components/CreateList.vue
+++ src/components/CreateList.vue
@@ -6,6 +6,7 @@ const emit = defineEmits(["activate"]);
 
 const showCreateListDialog = ref(false);
 const title = ref("");
+const description = ref("");
 
 function openModal() {
   showCreateListDialog.value = true;
@@ -18,9 +19,14 @@ function closeModal() {
   showCreateListDialog.value = false;
 }
 
-async function createList(title) {
-  const newId = await db.lists.add({ title: title });
-  const list = await db.lists.get({ id: newId });
+async function createList() {
+  // Insert the list in the db with title and descripton
+  const newId = await db.lists.add({
+    title: title.value,
+    description: description.value,
+  });
+  // Fetch the list to activate it
+  const list = await db.lists.get(newId);
   emit("activate", list);
   closeModal();
 }
@@ -31,12 +37,20 @@ async function createList(title) {
     <button class="add" @click.stop="openModal">Ny lista…</button>
     <div class="dialog create-list-dialog" v-if="showCreateListDialog">
       <input
+        class="margin-bottom"
         type="text"
         v-model="title"
-        @keyup.enter="createList(title)"
+        @keyup.enter="createList()"
         @keyup.esc="closeModal"
         placeholder="Skriv namn på listan…"
       />
+      <textarea
+        id="description"
+        v-model="description"
+        cols="30"
+        rows="10"
+        placeholder="Ev tävlingsregler, syften med listan, etc"
+      ></textarea>
       <div>
         <button class="create" @click="createList(title)">Skapa lista</button>
         <button @click="closeModal">Avbryt</button>
