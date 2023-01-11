bd9576fc93c3526e77bbcc1c41be6eaa72923106
Anton Andreasson <anton.andreasson@tickster.com>
2023-01-11 11:42:37 +0100

    Add sw config file (used?)

---
+++ service-worker-src.js
@@ -0,0 +1,55 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js")
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+// cache name
+workbox.core.setCacheNameDetails({
+    prefix: 'birdlist-cache',
+    precache: 'precache',
+    runtime: 'runtime',
+});
+  
+// runtime cache
+// 1. stylesheet
+workbox.routing.registerRoute(
+    new RegExp('\.css$'),
+    workbox.strategies.cacheFirst({
+        cacheName: 'birdlist-cache-css',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 7, // cache for one week
+                maxEntries: 20, // only cache 20 request
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+// 2. images
+workbox.routing.registerRoute(
+    new RegExp('\.(png|svg|jpg|webp|avif)$'),
+    workbox.strategies.cacheFirst({
+        cacheName: 'birdlist-cache-img',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 7,
+                maxEntries: 50,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+// 3. cache Dexie Cloud result
+workbox.routing.registerRoute(
+    new RegExp('https://zyh2ho4s6.dexie.cloud/'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'birdlist-cache-content',
+        cacheExpiration: {
+            maxAgeSeconds: 60 * 10 //cache the content for 10 min
+        }
+    })
+);
+  
+workbox.precaching.precacheAndRoute([]);
