20eac4a85f2daa78a437db5b26123fb509ba2cce
Anton Andreasson <anton.andreasson@tickster.com>
2023-04-05 12:12:56 +0200

    Replace some stubs with WIP functions

--- index.js
+++ index.js
@@ -4,22 +4,24 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 
+const publicVapidKey = "BC-q_Qa_xZrCippKmu2_x6oRsJFP7E9II66LbGAvhUc_Hw2Xe9pm6JJFEj_07OJzIcI4NjU4ovz8oOKb1jqPyhU";
+const privateVapidKey = "umUxE_qhAiU-GD4e68jsI9eDktH3PiYRgEbUcqbv1bE";
+const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiaXJkbGlzdEBzeXN0ZW0ubG9jYWwiLCJzY29wZXMiOlsiQUNDRVNTX0RCIl0sImlhdCI6MTY4MDYzNjc0NSwibmJmIjoxNjgwNjM2NDQ1LCJleHAiOjE2ODA2NDAzNDUsImF1ZCI6WyJodHRwczovL3p5aDJobzRzNi5kZXhpZS5jbG91ZCIsInp5aDJobzRzNiJdLCJpc3MiOiJEYXZpZElzc3VlciJ9.NTiByo-GbQnpj2-T-ID4TfDZZSffHuEG7re75uBPlQM";
+const accessHeaders = { "Authorization": "Bearer " + accessToken }
+
 // Create express app.
 const app = express();
 
 // Add CORS
 app.use(cors({
   origin: ['http://localhost:4173','http://localhost:5173']
-}));
+}));  
 
 // Use body parser which we will use to parse request body that sending from client.
 app.use(bodyParser.json());
 
 // We will store our client files in ./client directory.
 app.use(express.static(path.join(__dirname, "client")))
-
-const publicVapidKey = "BC-q_Qa_xZrCippKmu2_x6oRsJFP7E9II66LbGAvhUc_Hw2Xe9pm6JJFEj_07OJzIcI4NjU4ovz8oOKb1jqPyhU";
-const privateVapidKey = "umUxE_qhAiU-GD4e68jsI9eDktH3PiYRgEbUcqbv1bE";
 
 // Setup the public and private VAPID keys to web-push library.
 webpush.setVapidDetails("mailto:anton@andreasson.org", publicVapidKey, privateVapidKey);
@@ -55,15 +57,24 @@ function saveSubscriptionToDatabase(subscription) {
 }
 
 function insertToDatabase(subscription, callback) {
-  //stub
+  fetch("https://zyh2ho4s6.dexie.cloud/my/webPushSubscriptions", {
+    method: "POST",
+    headers: accessHeaders
+  }).then(response => console.log(response));
 }
 
 function deleteSubscriptionFromDatabase(id) {
-  //stub
+  fetch(`https://zyh2ho4s6.dexie.cloud/my/webPushSubscriptions/${id}`, {
+    method: "DELETE",
+    headers: accessHeaders
+  }).then(response => console.log(response));
 }
 
 function getSubscriptionsFromDatabase() {
-  //stub
+  fetch("https://zyh2ho4s6.dexie.cloud/my/webPushSubscriptions", {
+    method: "GET",
+    headers: accessHeaders
+  }).then(response => console.log(response));
 }
 
 function triggerPush(subscription, dataToSend) {
