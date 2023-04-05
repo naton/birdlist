a7315725c3858c7f05a9952b1ceaf7023aca7f20
Anton Andreasson <anton.andreasson@tickster.com>
2023-04-05 11:05:46 +0200

    Modify SW with support for push notifications

--- service-worker-src.js
+++ service-worker-src.js
@@ -54,13 +54,50 @@ workbox.routing.registerRoute(
   
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-// Push notifications
-self.addEventListener('push', function(e) {
-    const data = e.data.json();
-    self.registration.showNotification(
-        data.title,
-        {
-            body: data.body,
+console.log("Service worker loaded from sw.js...");
+
+// Push notifications (via https://blog.lekoala.be/the-only-snippet-you-will-need-to-deal-with-push-notifications-in-a-service-worker)
+self.addEventListener('push', function(event) {
+  if (!event.data) {
+    console.log('This push event has no data.');
+    return;
+  }
+  if (!self.registration) {
+    console.log('Service worker does not control the page');
+    return;
         }
-    );
+  if (!self.registration || !self.registration.pushManager) {
+    console.log('Push is not supported');
+    return;
+  }
+  const eventText = event.data.text();
+  // Specify default options
+  let options = {};
+  let title = '';
+
+  // Support both plain text notification and json
+  if (eventText.substr(0, 1) === '{') {
+    const eventData = JSON.parse(eventText);
+    title = eventData.title;
+
+    // Set specific options
+    // @link https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification#parameters
+    if (eventData.options) {
+      options = Object.assign(options, eventData.options);
+    }
+
+    // Check expiration if specified
+    if (eventData.expires && Date.now() > eventData.expires) {
+      console.log('Push notification has expired');
+      return;
+    }
+  } else {
+    title = eventText;
+  }
+  // Warning: this can fail silently if notifications are disabled at system level
+  // The promise itself resolve to undefined and is not helpful to see if it has been displayed properly
+  const promiseChain = self.registration.showNotification(title, options);
+
+  // With this, the browser will keep the service worker running until the promise you passed in has settled.
+  event.waitUntil(promiseChain);
 });
