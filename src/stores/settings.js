f5e32834e335a2b4868aa9a7e3ce962ea24de5f0
Anton Andreasson <anton.andreasson@tickster.com>
2024-03-24 16:46:28 +0100

    Sync theme color with user choice

--- src/stores/settings.js
+++ src/stores/settings.js
@@ -3,16 +3,14 @@ import { defineStore } from "pinia";
 import { useObservable } from "@vueuse/rxjs";
 import { db } from "../db";
 
-export const useSettingsStore = defineStore(
-  "settings",
-  () => {
-    const lang = ref("sv");
+export const useSettingsStore = defineStore("settings", () => {
+    const lang = ref(navigator.language.substring(0, 2));
     const hue = ref("45");
     const texts = ref({});
     const currentUser = useObservable(db.cloud.currentUser);
     const currentYear = ref(new Date().getFullYear());
     const currentMonth = ref(new Date().getMonth());
-  
+
     function t(l10n_key, ...args) {
       const l10n = texts.value[l10n_key];
 
@@ -27,13 +25,28 @@ export const useSettingsStore = defineStore(
       texts.value = (await import(`@/assets/texts_${lang}.json`)).default[0];
     }
 
+    function getThemeColor() {
+      return window.getComputedStyle(document.querySelector(".body-nav")).backgroundColor;
+    }
+
+    function setThemeColor() {
+      document.querySelector('meta[name="theme-color"]').content = getThemeColor();
+    }
+
     watch(lang, (newLang) => {
       loadTexts(newLang);
     });
 
     watch(hue, (newHue) => {
       document.documentElement.style = `--hue: ${newHue}`;
+      setThemeColor();
     });
+
+    var matchMediaDark = window.matchMedia("(prefers-color-scheme: dark)");
+    var matchMediaLight = window.matchMedia("(prefers-color-scheme: light)");
+
+    matchMediaDark.addEventListener("change", (e) => setThemeColor());
+    matchMediaLight.addEventListener("change", (e) => setThemeColor());
 
     return {
       lang,
