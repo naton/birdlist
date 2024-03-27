68b4e50767c2d8d60ce6bb5cdb02fe3261f083a4
Anton Andreasson <anton.andreasson@tickster.com>
2024-03-27 19:27:57 +0100

    Load About page dynamically

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import FriendsView from "../views/FriendsView.vue";
 import SettingsView from "../views/SettingsView.vue";
-import AboutView from "../views/AboutView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,7 +24,7 @@ const router = createRouter({
     {
       path: "/about",
       name: "about",
-      component: AboutView,
+      component: () => import("../views/AboutView.vue"),
     },
   ],
 });
